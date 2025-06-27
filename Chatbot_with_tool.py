bdefcd96c248481beb01d044b66f72d61782faae
Muhammad Tayyab <tyb3122@gmail.com>
2025-06-27 16:28:46 +0500

    _

--- Chatbot_with_tool.py
+++ Chatbot_with_tool.py
@@ -15,36 +15,49 @@ from typing import Annotated
 from typing_extensions import TypedDict
 from langgraph.graph import StateGraph, START , END
 from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, tools_condition
+
+from langchain_community.utilities import ArxivAPIWrapper, WikipediaAPIWrapper
+from langchain_community.tools import ArxivQueryRun , WikipediaQueryRun
+
+
+arxiv_wrapper = ArxivAPIWrapper(top_k_results=2,doc_content_chars_max=400)
+
+tool_arxiv = ArxivQueryRun(api_wrapper=arxiv_wrapper)
+
+
+wiki_wrapper = WikipediaAPIWrapper(top_k_results=2,doc_content_chars_max=400)
+
+tool_wiki = WikipediaQueryRun(api_wrapper=wiki_wrapper)
+
+
+tools = [tool_wiki]
+
+#Langgraph application
+llm = init_chat_model("llama3-8b-8192", model_provider="groq")
+llm = llm.bind(tools=tools)
 
 class State(TypedDict):
-    """
-    Messages have the type "list" The "add_messages" function
-    in the annotation defines how this state key should updates
-    (in our case , it appends messages to the list ratHer than overwriting them)
-
-    """
-
     messages:Annotated[list,add_messages]
+
+def chatbot(state:State):
+    return {"messages": llm.invoke(state["messages"])}
 
 graph_builder = StateGraph(State)
 
-
-
-def chatbot(state:State):
-    return {"messages":llm.invoke(state["messages"])}
-
 graph_builder.add_node("chatbot",chatbot)
 graph_builder.add_edge(START,"chatbot")
-graph_builder.add_edge("chatbot",END)
+tool_node = ToolNode(tools=tools)
+graph_builder.add_node("tools", tool_node)
+
+graph_builder.add_conditional_edges(
+    "chatbot",
+    tools_condition,
+)
+graph_builder.add_edge("tools", "chatbot")
+
 
 graph = graph_builder.compile()
-
-# from IPython.display import Image, display
-# try:
-#     display(Image(graph.get_graph().draw_mermaid_png()))
-# except Exception:
-#     pass
-
 
 state = {"messages": []}
 
@@ -53,13 +66,8 @@ while True:
     if user_input.lower() in ["quit", "q"]:
         print("Good Bye!")
         break
-
     state["messages"].append(("user", user_input))
-
     for event in graph.stream(state):
-        # print(state["messages"])
-        # print(state)
-        # print(event.values())
         for value in event.values():
             state["messages"].append(("assistant", value["messages"].content))
             print("Assistant:", value["messages"].content)
